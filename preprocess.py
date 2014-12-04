522dbb701e87b504ec3926ff020c880a4ff87223
Harini Kannan <hkannan@mit.edu>
2014-12-04 01:35:52 -0500

    added preprocessing

--- preprocess.py
+++ preprocess.py
@@ -12,7 +12,7 @@ bad = 'BadImag/Bmp/'
 good = 'GoodImg/Bmp/'
 
 #turn this flag on if we want to test on a harder data set
-USE_BAD = false
+USE_BAD = False
 
 training_data = []
 training_labels = []
@@ -21,7 +21,8 @@ validation_labels = []
 testing_data = []
 testing_labels = []
 
-for i in range (1, 63):
+# Range endpoint should be 63, but leaving it as 2 for testing purposes.
+for i in range (1, 2):
 	num = ''
 	if i < 10:
 		num = str(0) + str(i) + '/'
@@ -37,24 +38,24 @@ for i in range (1, 63):
 		directory = root + data_set + "Sample0" + num
 		files = os.listdir(directory)
 		endpoints = []
-		endpoints[0] = len(files)/3
-		endpoints[1] = 2*len(files)/3
+		endpoints.append(len(files)/3)
+		endpoints.append(2*len(files)/3)
 
 		for j in range(len(files)):
-			filename = files[i]
+			filename = directory + files[i]
 			img = Image.open(open(filename))
 
 			# convert to B&W, not sure if this works yet
-			img = img.convert('1')
+			img = img.convert('L')
 			img = numpy.asarray(img, dtype='float64') / 256
 
 			if j >= 0 and j < endpoints[0]:
 				training_data.append(img)
 				training_labels.append(i)
-			else if j >= endpoints[0] and j < endpoints[1]:
+			elif j >= endpoints[0] and j < endpoints[1]:
 				validation_data.append(img)
 				validation_labels.append(i)
-			else if j >= endpoints[1] and j < endpoints[2]:
+			elif j >= endpoints[1]:
 				testing_data.append(img)
 				testing_labels.append(i)
 
@@ -66,7 +67,8 @@ testing_data = numpy.asarray(testing_data)
 testing_labels = numpy.asarray(testing_labels)
 
 final_data_set = ((training_data, training_labels), (validation_data, validation_labels), (testing_data, testing_labels))
-cPickle.dump(final_data_set, 'single_char_data.pkl')
+print final_data_set[0][0][0][len(final_data_set[0][0][0]) - 1]
+#cPickle.dump(final_data_set, 'single_char_data.pkl')
 
 # don't forget to zip!
 
